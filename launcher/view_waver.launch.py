10d9bdb73af2a8928ece50821267ce23bb5c57a8
Alejandro Gomez <alejodiscovery20@gmail.com>
2025-02-03 02:50:03 -0500

    chore: first files

---
+++ launcher/view_waver.launch.py
@@ -0,0 +1,65 @@
+import os
+import xacro
+from pathlib import Path
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch.actions import RegisterEventHandler, SetEnvironmentVariable
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    # Launch Arguments
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    waver_description_path = os.path.join(
+        get_package_share_directory('waver_description'),
+        'urdf',
+        'waver.xacro'
+    )
+
+    # Load the URDF file (change to xacro if needed)
+    doc = xacro.process_file(waver_description_path)
+    robot_desc = doc.toprettyxml(indent='  ')
+
+    # Robot State Publisher
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{"robot_description": robot_desc, "use_sim_time": use_sim_time}]
+    )
+
+    # Joint State Publisher GUI (optional, for interactive joint control)
+    joint_state_publisher_gui = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        output='screen'
+    )
+
+    # Rviz Configuration
+    rviz_config_file = os.path.join(
+        get_package_share_directory('waver_description'),
+        'rviz',
+        'config.rviz'
+    )
+
+    rviz = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=["-d", rviz_config_file]
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument('use_sim_time', default_value='false', description='Use simulation (Gazebo) clock'),
+        robot_state_publisher,
+        joint_state_publisher_gui,  # Only needed for interactive joints
+        rviz
+    ])
